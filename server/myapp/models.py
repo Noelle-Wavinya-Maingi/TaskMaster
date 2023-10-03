05271669faf5ebe8cff5a054e4da059c420b37ab
Noelle-Wavinya-Maingi <noelle.maingi@student.moringaschool.com>
2023-10-03 18:07:17 +0300

    Created the Task model.

--- server/myapp/models.py
+++ server/myapp/models.py
@@ -1,33 +1,48 @@
 from myapp import db
 from sqlalchemy_serializer import SerializerMixin
 
+
 class User(db.Model, SerializerMixin):
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String, unique = True, nullable = False)
-    email = db.Column(db.String(120), unique = True, nullable = False)
-    password = db.Column(db.String(255), nullable = False)
-    image_file = db.Column(db.String, nullable = False, default = "default.jpg")
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    image_file = db.Column(db.String, nullable=False, default="default.jpg")
 
-    task_lists = db.relationship('Task_List', backref = 'user', lazy = True)
-    
+    task_lists = db.relationship("Task_List", backref="user", lazy=True)
+
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-    
+
+
 class Task_List(db.Model, SerializerMixin):
-    __tablename__ = 'task_lists'
+    __tablename__ = "task_lists"
 
-    id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(100), nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
-    tasks = db.relationship('Task', backref = 'task_lists', lazy = True)
+    tasks = db.relationship("Task", backref="task_lists", lazy=True)
 
     def __repr__(self):
         return f"Task list {self. title} {self.description}"
 
-class Task(db.Model, SerializerMixin):
-    pass
 
+class Task(db.Model, SerializerMixin):
+    __tablename__ = "tasks"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String)
+    due_date = db.Column(db.DateTime)
+    updated_on = db.Column(db.DateTime, onupdate=db.func.now())
+    completed = db.Column(db.Boolean, default=False)
+    task_list_id = db.Column(db.Integer, db.ForeignKey("task_lists.id"), nullable=False)
+
+    labels = db.relationship("Label", secondary="task_labes", back_populates="tasks")
+
+    def __repr__(self):
+        return f"Task {self.title} {self.description} {self.completed} {self.due_date}"
