e05e104a19f3d8420087f356ec722093e1f5fbdf
Noelle-Wavinya-Maingi <noelle.maingi@student.moringaschool.com>
2023-10-04 22:24:25 +0300

    Added the comments.

--- server/myapp/routes.py
+++ server/myapp/routes.py
@@ -1,16 +1,21 @@
+# Import necessary modules
 from myapp import api, db, bcrypt
-from flask import request, jsonify,make_response
+from flask import request, jsonify, make_response
 from myapp.models import User, Task_List
 from flask_restful import Resource
-from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_required
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    get_jwt_identity,
+    jwt_required,
+)
 
-# task_lists = []
 
-
+# Define a resource for user registration
 class UserRegistrationResource(Resource):
     def post(self):
-        if request.is_json:
             try:
+                # Parse JSON data from the request body
                 data = request.get_json()
                 username = data.get("username")
                 email = data.get("email")
@@ -24,6 +29,7 @@ class UserRegistrationResource(Resource):
                 if existing_user:
                     return {"message": "Username already exists"}, 400
 
+                # Check if the email already exists
                 existing_email = User.query.filter_by(email=email).first()
                 if existing_email:
                     return {"message": "Email already exists"}, 400
@@ -45,72 +51,80 @@ class UserRegistrationResource(Resource):
                 return {"message": "User registered successfully"}, 201
             except Exception as e:
                 return {"message": str(e)}, 500
-        else:
-            return {"message": "Request Content-Type must be 'application/json'"}, 400
 
+
+
+# Define a resource for user login
 class UserLoginResource(Resource):
     def post(self):
         if request.is_json:
             try:
+                # Parse JSON data from the request body
                 data = request.get_json()
                 username = data.get("username")
                 password = data.get("password")
 
                 if not username or not password:
                     return {"message": "Missing required fields."}
-                
-                user = User.query.filter_by(username = username).first()
+
+                # Query the database for the user by username
+                user = User.query.filter_by(username=username).first()
 
                 if not user:
                     return {"message": "User not found!"}, 404
 
+                # Check if the provided password matches the hashed password
                 if bcrypt.check_password_hash(user.password, password):
-                    access_token = create_access_token(identity = user.id)
+                    # Create access and refresh tokens for the user
+                    access_token = create_access_token(identity=user.id)
+                    refresh_token = create_refresh_token(identity=user.id)
 
-                    refresh_token = create_refresh_token(identity = user.id)
-
-                    return {"message": "Login Successful", "access_token" : access_token, "refresh_token": refresh_token}, 200
-                
+                    return {
+                        "message": "Login Successful",
+                        "access_token": access_token,
+                        "refresh_token": refresh_token,
+                    }, 200
                 else:
                     return {"message": "Invalid Login!"}, 401
-                
             except Exception as e:
                 return {"message": str(e)}, 500
-
         else:
             return {"message": "Request Content-Type must be 'application/json'"}, 400
 
 
+# Define a resource for managing task lists
 class TaskLists(Resource):
     @jwt_required()
     def get(self):
         try:
+            # Get the current user's identity from the JWT
             current_user_id = get_jwt_identity()
 
+            # Query the database for task lists associated with the current user
             task_lists = Task_List.query.filter_by(user_id=current_user_id).all()
 
+            # Serialize the task lists data
             task_lists_data = []
 
             for task_list in task_lists:
                 task_list_data = {
-                    "id" : task_list.id,
-                    "title" : task_list.title,
-                    "description" : task_list.description,
-                    "user_id" : task_list.user_id,
+                    "id": task_list.id,
+                    "title": task_list.title,
+                    "description": task_list.description,
+                    "user_id": task_list.user_id,
                 }
                 task_lists_data.append(task_list_data)
 
-                response_dict = {"message" : "Task lists retrieved successfully", "task_lists" : task_lists_data,}
+            response_dict = {
+                "message": "Task lists retrieved successfully",
+                "task_lists": task_lists_data,
+            }
 
-                response = make_response(jsonify(response_dict), 200)
-
+            response = make_response(jsonify(response_dict), 200)
         except Exception as e:
-            response_dict = {"errors" : str(e)}
-
+            response_dict = {"errors": str(e)}
             response = make_response(jsonify(response_dict), 500)
-
         return response
-    
 
     @jwt_required()
     def post(self):
@@ -127,14 +141,12 @@ class TaskLists(Resource):
                 response_dict = {"errors": ["Title is required"]}
                 return make_response(jsonify(response_dict), 400)
 
-            # Create a new TaskList instance
+            # Create a new Task_List instance
             task_list = Task_List(
-                title=title,
-                description=description,
-                user_id=current_user_id
+                title=title, description=description, user_id=current_user_id
             )
 
-            # Add and commit the new TaskList to the database
+            # Add and commit the new Task_List to the database
             db.session.add(task_list)
             db.session.commit()
 
@@ -145,26 +157,21 @@ class TaskLists(Resource):
                     "id": task_list.id,
                     "title": task_list.title,
                     "description": task_list.description,
-                    "user_id": task_list.user_id
-                }
+                    "user_id": task_list.user_id,
+                },
             }
 
-            response = make_response(jsonify(response_dict), 201)  # Use 201 Created status code
-
+            response = make_response(
+                jsonify(response_dict), 201
+            )  
         except Exception as e:
             # Handle any exceptions that may occur during the creation process
             response_dict = {"errors": ["An error occurred: " + str(e)]}
             return make_response(jsonify(response_dict), 500)
-
         return response
 
 
-    
-
-
-
-# Specify that only POST requests are allowed for this route
+# Specify the routes and resources
 api.add_resource(UserRegistrationResource, "/register")
 api.add_resource(UserLoginResource, "/login")
-api.add_resource(TaskLists, '/tasklist')
-
+api.add_resource(TaskLists, "/tasklist")
